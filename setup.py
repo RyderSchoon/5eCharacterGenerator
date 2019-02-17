c949c26010b79c8ab052d9ab71f6d8c399869e47
Acer Laptop\CanadianLobster <rdschoon@iastate.edu>
2019-02-17 16:37:49 -0600

    Refactoring project name and app name added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='5eCharacterGenerator',
+    version='0.1dev',
+    packages=['5eCharacterGenerator',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
--- tests/test_name_service.py
+++ tests/test_name_service.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../microblog/app/')
+sys.path.insert(0, myPath + '/../5eCharacterGenerator/app/')
 
 from viewModels.characterModel import CharacterModel
 from services.nameService import NameService
