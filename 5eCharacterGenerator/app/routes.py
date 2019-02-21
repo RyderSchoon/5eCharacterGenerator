f338ba3654c549c7c78eef84c7fdb5d7b6d3acee
Acer Laptop\CanadianLobster <rdschoon@iastate.edu>
2019-02-20 20:41:12 -0600

    Swapped to using azure mysql database

--- 5eCharacterGenerator/app/routes.py
+++ 5eCharacterGenerator/app/routes.py
@@ -3,9 +3,10 @@ from flask import render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from services.characterService import CharacterService
 from viewModels.characterModel import CharacterModel
 
-engine = create_engine('mysql://root:root@localhost:3306/character_generator')
+engine = create_engine('mysql://root_user@5echaractergenerator.mysql.database.azure.com:Adi!ste08@5echaractergenerator.mysql.database.azure.com:3306/character_generator')
 engine.connect()
 
 
@@ -16,4 +17,5 @@ def index():
     session = session_maker()
 
     character = CharacterModel(session)
+    CharacterService.randomize_character(character)
     return render_template('5e attempt.html', character=character)
